140e16eeb888c7cddfdbb601e86e28e43fedec7a
Rajan-rkg9 <amirajangupta@gmail.com>
2020-10-28 12:52:59 +0530

    [Rajan-rkg9] Insert. Values entered into table

---
+++ INSERT VALUES INTO TABLE.sql
@@ -0,0 +1,5 @@
+insert into Address_Book (first_name, last_name, address, city, state, zip, phone_number, email) values ('Rajan Kumar','Gupta','Panki','Panki','Jharkhand','822122','9876543201','rajangupta@gmail.com'),
+				 ('Rohan','Kuumar','Patan','Palamu','Jharkhand','823122','9876543201','irohapta@gmail.com'),
+                 ('Sumit','Kumar','Ranchi','Ranchi','Jharkhand','824022','9897654301','sumit@gmail.com'),
+                 ('Shubham','Tiwari','Bareilly','Etawah','Uttar Pradesh','823428','7896543201','tiwrijii@gmail.com');
+select * from Address_Book;
